17e39175fd1d6bd85f05a4775e9537d05cedf07a
carloslazaroam <carloslazaroam@gmail.com>
2024-04-05 11:45:13 +0200

    Cambios mínimos, admin cuenta y usuarios ya hechos

--- client/CUENTA/Cuenta-Admin/scripts.js
+++ client/CUENTA/Cuenta-Admin/scripts.js
@@ -14,32 +14,40 @@ function inicio(cuentas) {
     console.log(cuentas);
     wrapper.innerHTML = "";
     cuentas.forEach(cuenta => {
-        wrapper.innerHTML += `
-        
-            <tr>
-                <td style="font-family: Imaki;margin: 0 auto">${cuenta.id}</td>
-                <td>${formatDate(cuenta.fechacreacion)}</td>
-                <td>${cuenta.activa}</td>
-                <td>${cuenta.iban}</td>
-                
-                <td>
-                    <button id="botonEliminar" onclick="confirmarEliminacion('${cuenta.iban}')">
-                        <?xml version="1.0" encoding="UTF-8"?>
-                        <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-                            <path d="M20 9L18.005 20.3463C17.8369 21.3026 17.0062 22 16.0353 22H7.96474C6.99379 22 6.1631 21.3026 5.99496 20.3463L4 9" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M21 6L15.375 6M3 6L8.625 6M8.625 6V4C8.625 2.89543 9.52043 2 10.625 2H13.375C14.4796 2 15.375 2.89543 15.375 4V6M8.625 6L15.375 6" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
-                    </button>
-                    <button id="botonEditar" onclick="editarCuenta('${cuenta.iban}', '${cuenta.fechacreacion}', '${cuenta.activa}')">
-                        <?xml version="1.0" encoding="UTF-8"?>
-                        <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-                            <path d="M14.3632 5.65156L15.8431 4.17157C16.6242 3.39052 17.8905 3.39052 18.6716 4.17157L20.0858 5.58579C20.8668 6.36683 20.8668 7.63316 20.0858 8.41421L18.6058 9.8942M14.3632 5.65156L4.74749 15.2672C4.41542 15.5993 4.21079 16.0376 4.16947 16.5054L3.92738 19.2459C3.87261 19.8659 4.39148 20.3848 5.0115 20.33L7.75191 20.0879C8.21972 20.0466 8.65806 19.8419 8.99013 19.5099L18.6058 9.8942M14.3632 5.65156L18.6058 9.8942" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
-                    </button>
-                </td>
-            </tr>`;
+        // Obtener el nombre y primer apellido del usuario correspondiente
+        fetch(recurso + '/users/' + cuenta.id_usuario)
+            .then(res => res.json())
+            .then(usuario => {
+                // Mostrar el nombre y primer apellido del usuario en lugar del ID
+                wrapper.innerHTML += `
+                    <tr>
+                        <td style="font-family: Imaki;margin: 0 auto">${cuenta.id}</td>
+                        <td>${formatDate(cuenta.fechacreacion)}</td>
+                        <td>${cuenta.id_usuario}</td>
+                        <td>${cuenta.activa}</td>
+                        <td>${cuenta.iban}</td>
+                        
+                        <td>
+                            <button id="botonEliminar" onclick="confirmarEliminacion('${cuenta.iban}')">
+                            <?xml version="1.0" encoding="UTF-8"?>
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                                <path d="M20 9L18.005 20.3463C17.8369 21.3026 17.0062 22 16.0353 22H7.96474C6.99379 22 6.1631 21.3026 5.99496 20.3463L4 9" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                <path d="M21 6L15.375 6M3 6L8.625 6M8.625 6V4C8.625 2.89543 9.52043 2 10.625 2H13.375C14.4796 2 15.375 2.89543 15.375 4V6M8.625 6L15.375 6" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                            </button>
+                            <button id="botonEditar" onclick="editarCuenta('${cuenta.iban}', '${cuenta.fechacreacion}', '${cuenta.activa}','${cuenta.id_usuario}','${cuenta.id_tipocuenta}')">
+                            <?xml version="1.0" encoding="UTF-8"?>
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                                <path d="M14.3632 5.65156L15.8431 4.17157C16.6242 3.39052 17.8905 3.39052 18.6716 4.17157L20.0858 5.58579C20.8668 6.36683 20.8668 7.63316 20.0858 8.41421L18.6058 9.8942M14.3632 5.65156L4.74749 15.2672C4.41542 15.5993 4.21079 16.0376 4.16947 16.5054L3.92738 19.2459C3.87261 19.8659 4.39148 20.3848 5.0115 20.33L7.75191 20.0879C8.21972 20.0466 8.65806 19.8419 8.99013 19.5099L18.6058 9.8942M14.3632 5.65156L18.6058 9.8942" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                            </button>
+                        </td>
+                    </tr>`;
+            })
+            .catch(err => console.error('Error al obtener usuario:', err));
     });
 }
+
 
 // Función para formatear la fecha en un formato legible por humanos
 function formatDate(dateString) {
@@ -61,10 +69,10 @@ function confirmarEliminacion(iban) {
 
 let cuentaEditando = null;
 
-function editarCuenta(iban, activa) {
-    cuentaEditando = { iban, activa };
-    
-    document.getElementById('editActiva').checked = activa; 
+function editarCuenta(iban, activa, id_usuario,id_tipocuenta) {
+    cuentaEditando = { iban, activa, id_usuario,id_tipocuenta };
+
+    document.getElementById('editActiva').checked = activa;
     document.getElementById('editIban').value = iban;
 
     // Llenar el campo editUsuario con opciones disponibles y seleccionar el usuario actual
@@ -78,18 +86,38 @@ function editarCuenta(iban, activa) {
                 const option = document.createElement('option');
                 option.value = user._id;
                 option.textContent = user.nombre + ' ' + user.apellido1 + ' ' + user.apellido2;
+                if (user._id.toString() === id_usuario.toString()) {
+                    option.selected = true; // Seleccionar automáticamente el usuario asignado a la cuenta
+                }
                 select.appendChild(option);
             });
-
-            // Obtener el id_usuario de la cuenta actual y seleccionarlo en el desplegable
-            fetch(recurso + '/cuentas/' + cuentaEditando.iban)
-                .then(res => res.json())
-                .then(cuenta => {
-                    document.getElementById('editUsuario').value = cuenta.id_usuario;
-                })
-                .catch(err => console.error('Error al obtener la cuenta:', err));
         })
         .catch(err => console.error('Error al obtener usuarios:', err));
+
+        // Llenar el campo editTipoCuenta con opciones disponibles y seleccionar el tipo de cuenta actual
+        fetch(recurso + '/tipocuentas')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('No se pudieron obtener los tipos de cuenta');
+            }
+            return res.json();
+        })
+        .then(tiposCuenta => {
+            console.log("Tipos de cuenta obtenidos:", tiposCuenta);
+            const selectTipoCuenta = document.getElementById('editTipocuenta');
+            selectTipoCuenta.innerHTML = "";
+            tiposCuenta.forEach(tipo => {
+                const option = document.createElement('option');
+                option.value = tipo._id;
+                option.textContent = tipo.nombre;
+                if (cuentaEditando && cuentaEditando.id_tipocuenta && tipo._id.toString() === cuentaEditando.id_tipocuenta.toString()) {
+                    option.selected = true;
+                }
+                selectTipoCuenta.appendChild(option);
+            });
+        })
+        .catch(err => console.error('Error al obtener tipos de cuenta:', err));
+    
 
     const modal = document.getElementById('modal');
     modal.style.display = 'block';
@@ -99,10 +127,14 @@ function editarCuenta(iban, activa) {
 
 
 
+
+
 function guardarEdicion() {
    
     const newActiva = document.getElementById('editActiva').checked; // Obtener el valor booleano de la casilla de verificación
     const newIban = document.getElementById('editIban').value;
+    const newUsuario = document.getElementById('editUsuario').value;
+    const newTipocuenta = document.getElementById('editTipocuenta').value;
      
     fetch(recurso + '/cuentas/' + cuentaEditando.iban, {
         method: 'PUT',
@@ -112,7 +144,9 @@ function guardarEdicion() {
         body: JSON.stringify({
           
             activa: newActiva, // Asignar el valor booleano al campo "activa"
-            iban: newIban
+            iban: newIban,
+            id_usuario: newUsuario,
+            id_tipocuenta: newTipocuenta
         })
     })
     .then(res => {
@@ -166,12 +200,14 @@ function eliminarCuenta(iban) {
 function guardarNuevaCuenta() {
     const activa = document.getElementById('createActiva').checked;
     const iban = document.getElementById('createIBAN').value;
-    const idUsuario = document.getElementById('createUsuario').value; // Obtener el id del usuario seleccionado
+    const idUsuario = document.getElementById('createUsuario').value;
+    const idTipocuenta = document.getElementById('createTipocuenta').value; // Obtener el id del usuario seleccionado
 
     const cuentaData = {
         activa: activa,
         iban: iban,
-        id_usuario: idUsuario // Incluir el id del usuario en los datos de la cuenta
+        id_usuario: idUsuario,
+        id_tipocuenta: idTipocuenta // Incluir el id del usuario en los datos de la cuenta
     };
 
     fetch(recurso + '/cuentas/post', {
@@ -195,6 +231,7 @@ function guardarNuevaCuenta() {
         document.getElementById('createActiva').checked = false;
         document.getElementById('createIBAN').value = '';
         document.getElementById('createUsuario').value = '';
+        document.getElementById('createTipocuenta').value = '';
 
         const crearModal = document.getElementById('crearModal');
         crearModal.style.display = 'none';
@@ -248,6 +285,32 @@ function mostrarFormulario() {
             });
         })
         .catch(err => console.error('Error al obtener usuarios:', err));
+
+
+        fetch(recurso + '/tipocuentas')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('No se pudieron obtener los tipos de cuenta');
+            }
+            return res.json();
+        })
+        .then(tiposCuenta => {
+            console.log("Tipos de cuenta obtenidos:", tiposCuenta);
+            const selectTipoCuenta = document.getElementById('createTipocuenta');
+            selectTipoCuenta.innerHTML = "";
+            tiposCuenta.forEach(tipo => {
+                const option = document.createElement('option');
+                option.value = tipo._id;
+                option.textContent = tipo.nombre;
+                if (cuentaEditando && cuentaEditando.id_tipocuenta && tipo._id.toString() === cuentaEditando.id_tipocuenta.toString()) {
+                    option.selected = true;
+                }
+                selectTipoCuenta.appendChild(option);
+            });
+        })
+        .catch(err => console.error('Error al obtener tipos de cuenta:', err));
+
+
 }
 
 
